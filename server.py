cb6dd9ef7d32a535fffdd0ace04adea3b4a651d8
SM <incessantsoul@hotmail.com>
2013-04-28 17:20:31 -0700

    Milestone2. Added statichtmlpage file to checkin

--- server.py
+++ server.py
@@ -17,7 +17,7 @@ class HttpServer:
 
 	def isAcceptingConnections(self):
 		if self.numConnections ==3:
-			print "Max Connections reached"
+			print "Max Connections reached. Exiting."
 			return False
 		else:
 			return True
@@ -25,7 +25,7 @@ class HttpServer:
 	def start(self):
 		while self.isAcceptingConnections():
 			if self.sock:
-				print "Accepting Connections..."
+				print "\nAccepting Connections..."
 				(remoteSock, address ) = self.sock.accept()
 				if (remoteSock):
 					self.processConnection(remoteSock, address)
@@ -42,9 +42,24 @@ class HttpServer:
 
 
 	def processConnection(self, remoteSock, remoteAddress):
-		print "Recd: sock: %s addr:%s"  % (str(remoteSock), str(remoteAddress)) 	
+		print "Rx: addr:%s"  % (str(remoteAddress), ) 	
+		rStr = remoteSock.recv(1000)
+		rStr = rStr.split()
+		if rStr[0] and rStr[1] and rStr[2]:
+			print "Rx:", rStr[0], rStr[1], rStr[2]
+			if rStr[0] == "GET" and rStr[1] =="/":
+				staticF = open(SERVER_STATIC_HTML)
+				if not staticF:
+					print "static file invalid"
+				else:
+		#			print "staticFile:", staticF
+					for line in staticF:
+						bytes =remoteSock.send(line)
+				staticF.close()
+
 		remoteSock.close()
 		self.numConnections+=1
+
 
 if __name__ == "__main__":
 	server = HttpServer()
